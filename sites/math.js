0b877a25b771050ca2d4e763b7285488fde8c255
giram456123 <132762340+giram456123@users.noreply.github.com>
2023-05-06 19:49:17 +0600

    Add files via upload

---
+++ sites/math.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
+function muliply(a, b) {
+    return a * b;
+}
+function division(a, b) {
+    return a / b;
+}
